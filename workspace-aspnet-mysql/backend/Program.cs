6d536acbe4416448d7870378e210c2f674e89cc2
Douglasproglima <douglasproglima@gmail.com>
2021-08-02 21:49:28 -0300

    Update port MysqlAdmin

--- workspace-aspnet-mysql/backend/Program.cs
+++ workspace-aspnet-mysql/backend/Program.cs
@@ -37,9 +37,8 @@ public static void Main(string[] args)  => WebHost.CreateDefaultBuilder(args)
           using MySqlDataReader reader = cmd.ExecuteReader();
 
           while (reader.Read())
-          {
             titles.Add(reader.GetString(0));
-          }
+
           reader.Close();
         }
         catch (Exception ex)
@@ -67,15 +66,38 @@ public static void Main(string[] args)  => WebHost.CreateDefaultBuilder(args)
     connection.Open();
     using var transation = connection.BeginTransaction();
 
-    using MySqlCommand cmd1 = new MySqlCommand("DROP TABLE IF EXISTS blog", connection, transation);
+    using MySqlCommand cmd1 = 
+      new MySqlCommand(
+        "DROP TABLE IF EXISTS blog", 
+        connection, 
+        transation
+      );
+      
     cmd1.ExecuteNonQuery();
 
-    using MySqlCommand cmd2 = new MySqlCommand("CREATE TABLE IF NOT EXISTS blog (id int NOT NULL AUTO_INCREMENT, title varchar(255), PRIMARY KEY (id))", connection, transation);
+    using MySqlCommand cmd2 = 
+      new MySqlCommand(
+        @"CREATE TABLE IF NOT EXISTS blog (
+            id int NOT NULL AUTO_INCREMENT, 
+            title varchar(255), 
+            PRIMARY KEY (id)
+          )", 
+        connection, 
+        transation
+      );
+
     cmd2.ExecuteNonQuery();
     
     for (int i = 0; i < 5; i++)
     {
-      using MySqlCommand insertCommand = new MySqlCommand( $"INSERT INTO blog (title) VALUES ('Blog post #{i}');", connection, transation);
+      using MySqlCommand insertCommand = 
+        new MySqlCommand( 
+          $@"INSERT INTO blog (title) 
+            VALUES ('Blog post #{i}');", 
+          connection, 
+          transation
+        );
+
       insertCommand.ExecuteNonQuery();
     }
     transation.Commit();
